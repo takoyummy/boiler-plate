507a620d0c6bfbcdd4729a67e57156d7329c6f42
takoyummy <40001921+takoyummy@users.noreply.github.com>
2021-04-18 19:37:12 +0900

    두번째로  저장소에 올림

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ const express = require('express')
 const app = express()
 const port = 5000
 
+const mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://takoyummy:10115dkstpdk!@boilerplate.tbw7s.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+    useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
+}).then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log(err))
+
+
 app.get('/', (req, res) => {
   res.send('Hello World!~~안녕하세요')
 })
