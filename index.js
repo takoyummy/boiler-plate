486637ac694301acbd07a200059e90b05d7cbbf7
타코여미 <takoyummy@naver.com>
2021-04-23 17:42:35 +0900

    feat : update

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require('express')
 const app = express()
 const port = 5000
 const bodyParser = require('body-parser');
+const config = require('./config/key');
 const { User } = require("./models/User");
+
 
 //application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({extended: true}));
@@ -11,7 +13,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
-mongoose.connect('', {
+mongoose.connect('config.mongoURI', {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err))
